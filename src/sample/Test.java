ef420d2406b0abf6eb8cf5782d93aafd7063f923
“sansav123” <sandeep.savale555@gmail.com>
2026-07-28 17:29:00 +0530

    ss change

--- src/sample/Test.java
+++ src/sample/Test.java
@@ -3,5 +3,7 @@ package sample;
 public class Test {
     static void main(String[] args) {
         System.out.println("kanchan chormage");
+        System.out.println("Sandeep Savale");
+
     }
 }
