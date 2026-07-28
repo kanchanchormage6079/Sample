75485c9b7b587ea1545d7e684c2f8ef8aea9eca9
rutu9399 <rutujashinde931999@gmail.com>
2026-07-28 17:23:48 +0530

    rs chnage2

--- src/sample/Test.java
+++ src/sample/Test.java
@@ -4,5 +4,7 @@ public class Test {
     static void main(String[] args) {
         System.out.println("kanchan chormage");
         System.out.println("rutuja shinde");
+        System.out.println("kay chhalyy he");
+        System.out.println("mala kahi kalat nhi");
     }
 }
